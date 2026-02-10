ae6055477da8959b2775cceba7e3e608ee8c4a38
Virlouvet Evann <61600649+Virbek@users.noreply.github.com>
2026-02-10 15:57:19 +0100

    Day2 first half no opti

--- Day2/ver1/src/main.rs
+++ Day2/ver1/src/main.rs
@@ -1,4 +1,47 @@
+fn first_half(contenu : &'static str){
+    let mut limite = String::new();
+    let mut start = String::new();
+    let mut invalide : u64 = 0;
+
+    for c in contenu.chars(){
+        if c == ',' || c == '\n' {
+            let s : u32 = start.parse().unwrap();
+            let l : u32 = limite.parse().unwrap();
+            //println!("start : {}, limite : {}", s, l);
+            for i in s..=l {
+                let v = i.to_string();
+                if v.len() % 2 == 0{
+                    //println!("valeur : {}", v);
+                    start = v;
+                    limite = start.split_off(start.len()/2);
+                    let r1 : u32 = start.parse().unwrap();
+                    let r2 : u32 = limite.parse().unwrap();
+                    if r1 == r2 {
+                        start.push_str(&limite);
+                        let res :u64 = start.parse().unwrap();
+                        //println!("resultat : {}", res);
+                        invalide += res;
+                    }
+                } 
+            }
+            start.clear();
+            limite.clear();
+        }else{
+            if c == '-'{
+                start = limite.to_string();
+                limite.clear();
+            }else {
+                limite.push(c);
+            }
+        }
+        //println!("Mais que vaut C : {}", c);
+    }
+
+    println!("le resultat : {}", invalide);
+}
+
 fn main() {
     let contenu = include_str!("../ressources/code.txt");
-    
+    first_half(contenu);
+
 }
---
+++ Day2/ver1/target/debug/deps/ver1-693c08fd50a9bbdc.d
@@ -0,0 +1,6 @@
+C:\msys64\home\0203481A\AdventOfCodeRust5RVJV\Day2\ver1\target\debug\deps\ver1-693c08fd50a9bbdc.d: src\main.rs src\../ressources/code.txt
+
+C:\msys64\home\0203481A\AdventOfCodeRust5RVJV\Day2\ver1\target\debug\deps\ver1-693c08fd50a9bbdc.exe: src\main.rs src\../ressources/code.txt
+
+src\main.rs:
+src\../ressources/code.txt:
