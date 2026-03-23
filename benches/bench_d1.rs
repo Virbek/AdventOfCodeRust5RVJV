e55c3a9be4b87bf8a64aeff56f928aee87917f79
comeprz <come.przybyszewski@gmail.com>
2026-03-23 19:38:36 +0100

    Criterion D1

--- benches/bench_d1.rs
+++ benches/bench_d1.rs
@@ -20,3 +20,24 @@ ver 2 (Côme)
 
 Time (mean ± σ):       7.5 ms ±   0.9 ms    [User: 4.8 ms, System: 3.8 ms]
 Range (min … max):     6.4 ms …  13.0 ms    100 runs */
+
+// Criterion
+
+use std::hint::black_box;
+use criterion::{criterion_group, criterion_main, Criterion};
+use AdventOfCodeRust5RVJV::d1;
+
+fn bench_d1(c: &mut Criterion) {
+    let input = d1::INPUT;
+
+    c.bench_function("d1_v1_part1", |b| {
+        b.iter(|| d1::v1::solve_part1(black_box(input)))
+    });
+
+    c.bench_function("d1_v2_part1", |b| {
+        b.iter(|| d1::v2::solve_part1(black_box(input)))
+    });
+}
+
+criterion_group!(benches, bench_d1);
+criterion_main!(benches);
