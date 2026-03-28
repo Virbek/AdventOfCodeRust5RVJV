ef48350803adabf248f009e8574940bc31172aa9
comeprz <come.przybyszewski@gmail.com>
2026-03-28 12:21:50 +0100

    D3P2

--- benches/bench_d3.rs
+++ benches/bench_d3.rs
@@ -17,21 +17,21 @@ use AdventOfCodeRust5RVJV::d3;
 fn bench_d2(c: &mut Criterion) {
     let input = d3::INPUT;
 
-    c.bench_function("d3_v1_part1", |b| {
+    /*c.bench_function("d3_v1_part1", |b| {
         b.iter(|| d3::v1::solve_part1(black_box(input)))
     });
 
     c.bench_function("d3_v2_part1", |b| {
         b.iter(|| d3::v2::solve_part1(black_box(input)))
     });
-    /*
+    */
     c.bench_function("d3_v1_part2", |b| {
         b.iter(|| d3::v1::solve_part2(black_box(input)))
     });
 
     c.bench_function("d3_v2_part2", |b| {
         b.iter(|| d3::v2::solve_part2(black_box(input)))
-    });*/
+    });
 }
 
 criterion_group!(benches, bench_d2);
@@ -41,8 +41,9 @@ criterion_main!(benches);
 
 Partie 1
 d3_v1_part1             time:   [461.17 µs 462.99 µs 464.99 µs]
-d3_v2_part1             time:   [260.97 µs 261.59 µs 262.30 µs] => V2 2x plus perf
+d3_v2_part1             time:   [260.97 µs 261.59 µs 262.30 µs] => 2x plus perf
 
 Partie 2
-
- */
+d3_v1_part2             time:   [481.06 µs 488.25 µs 498.09 µs]
+d3_v2_part2             time:   [84.038 µs 84.252 µs 84.508 µs] => 5x plus perf
+*/
--- src/d3/v2.rs
+++ src/d3/v2.rs
@@ -20,22 +20,27 @@ pub fn solve_part1(input: &str) -> u32{
     sum
 }
 
-pub fn solve_part2(input: &str) -> u32{
-    let mut sum:u32 = 0;
+pub fn solve_part2(input: &str) -> u64{
+    let mut sum:u64 = 0;
 
     for ligne in input.lines() {
-        let chiffres: Vec<u32> = ligne.chars().map(|c| c.to_digit(10).unwrap()).collect();
-        let mut douze_max:u32 = 0;
+        let chiffres: Vec<u64> = ligne.chars().map(|c| c.to_digit(10).unwrap() as u64).collect();
+        let mut douze_max:u64 = 0;
+        let mut start = 0;
+        
+        for i in 0..12{
+            let mut val = 0;
+            let mut max_index = start;
+            let zone = chiffres.len() - (12-i);
 
-        // 987 898 898 898
-        for i in 0..chiffres.len(){
-            // i < j donc on part de i+1
-            for j in i+1..chiffres.len(){
-                let val = chiffres[i]*10 + chiffres[j];
-                if val > douze_max{
-                    douze_max = val;
+            for j in start..=zone{
+                if chiffres[j] > val{
+                    val = chiffres[j];
+                    max_index = j;
                 }
             }
+            douze_max = douze_max * 10 + val;
+            start = max_index+1;
         }
         sum += douze_max;
     }
